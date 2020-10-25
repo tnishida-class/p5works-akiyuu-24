a389e9fa0728c7021f2af31a390979d1153d706c
akiyuu-24 <72780570+akiyuu-24@users.noreply.github.com>
2020-10-25 19:12:42 +0900

    change section2

--- section2-q1/sketch.js
+++ section2-q1/sketch.js
@@ -1,6 +1,11 @@
 // 小手調べ
 function setup(){
+  createCanvas(100,100)
   for(let i = 0; i < 10; i++){
+  if(i<5) stroke(0,0,255);
+  else stroke(255,0,0);
+  noFill();
+ellipse(50, 50,i*5+5);   
     // BLANK[1]
   }
 }
--- section2-q2/sketch.js
+++ section2-q2/sketch.js
@@ -1,11 +1,66 @@
-// チェッカー
 function setup() {
+
   createCanvas(200, 200);
+  background(128);
+  let width = 200
   let size = width / 8;
-  noStroke();
-  for(let i = 0; i < 8; i++){
-    for(let j = 0; j < 8; j++){
-      // BLANK[1] (hint: rectのx座標は size * i, y座標はsize * j)
+
+  for(let i = 0; i < 9; i++){
+    for(let j = 0; j < 9; j++){
+
+
+        if(i % 2 == 0){
+          if(j % 2 == 0){
+            noStroke();
+            rect(i * size, j * size, size, size);
+          }
+        }
+
+        else if(i % 2 == 1){
+          if(j % 2 == 1){
+            noStroke();
+            rect(i * size, j * size, size, size);
+          }
+        }
+
+
+      }
     }
-  }
+
+
+
+
+    for(let i = 0; i < 9; i++){
+      for(let j = 0; j < 9; j++){
+
+
+        if(i % 2 == 0){
+          if(j == 1){
+            fill(255,0,0);
+            ellipse(i * size + (size / 2), size * (2 * j + 1) / 2, size * 7 / 8);
+          }
+          else if(j == 5 || j == 7 ){
+              fill(0,0,0);
+              ellipse(i * size + (size / 2), size * (2 * j + 1) / 2, size * 7 / 8);
+          }
+        }
+
+        else if(i % 2 == 1){
+          if(j == 0 || j == 2){
+            fill(255,0,0);
+            ellipse(i * size + (size / 2), size * (2 * j + 1) / 2, size * 7 / 8);
+          }
+          else if(j == 6){
+            fill(0,0,0);
+            ellipse(i * size + (size / 2), size * (2 * j + 1) / 2, size * 7 / 8);
+          }
+        }
+
+
+      }
+    }
+
+
+
+
 }

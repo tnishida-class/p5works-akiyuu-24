a602f66edaacca632e7ffc22ec239dc9a0be2a64
akiyuu-24 <72780570+akiyuu-24@users.noreply.github.com>
2020-11-08 14:14:15 +0900

    changesection3

--- section3-1/sketch.js
+++ section3-1/sketch.js
@@ -2,11 +2,24 @@
 // 練習問題：このプログラムを改造してEUの旗を描いてみよう
 function setup(){
   createCanvas(200, 200);
-  background(192);
+  background(0,69,181);
+  noStroke();
   for(let i = 0; i < 12; i++){
-    let theta = TWO_PI * i / 12;
-    let x = 100 + cos(theta) * 50;
-    let y = 100 + sin(theta) * 50;
-    ellipse(x, y, 10);
+    var theta = TWO_PI * i / 12;
+    var x = 100 + cos(theta) * 50;
+    var y = 100 + sin(theta) * 50;
+    fill(240,240,0);
+    star(x, y, 10);
   }
+
+function star(cx, cy, r){
+  beginShape();
+  for(let j = 0; j < 5; j++){
+    var theta = TWO_PI * j * 2/5 - HALF_PI;
+    var x = cx + cos(theta) * r;
+    var y = cy + sin(theta) * r;
+    vertex(x, y);
+  }
+  endShape(CLOSE);
 }
+  }
--- section3-3/sketch.js
+++ section3-3/sketch.js
@@ -1,7 +1,7 @@
 // テキスト「関数を作る(2) 値を戻す関数」～「総仕上げ：カレンダーを描画しよう」
 function setup(){
-  createCanvas(200, 200);
-  calendar(2019, 10);
+  createCanvas(400, 400);
+  calendar(2016,3);
 
   for(let i = 2000; i <= 2100; i++){
     if(isLeapYear(i)){
@@ -15,9 +15,58 @@ function setup(){
 
 function calendar(y, m){
   let dow = dayOfWeek(y, m, 1);
-  for(let d = 1; d <= daysInMonth(y, m); d++){
-    // BLANK[3] (hint: まずは daysInYear, dayOfWeek を作ろう)
+  let p = 20;
+  let t = 0;
+  let count = 0;
+  push();
+  fill(230,230,250);
+  rect(16,10,175,192);
+  pop();
+  line(16,53,190,53);
+  line(16,40,190,40);
+  line(40,40,40,202);
+  line(65,40,65,202);
+  line(90,40,90,202);
+  line(115,40,115,202);
+  line(140,40,140,202);
+  line(165,40,165,202);
+  line(16,77,190,77);
+  line(16,102,190,102);
+  line(16,126,190,126);
+  line(16,151,190,151);
+  line(16,178 ,190,178)
+;  textSize(15);
+  text(y + "年"+"  "+ m + "月", 58, 30);
+  textSize(9);
+  for(i = 0; i < 7; i++){
+    let a = dayOfWeekAsString(i);
+    text(a, 24.7* (i + 1), 50);
+
+
   }
+
+
+  for(let d = 1; d <= daysInMonth(y, m); d++){
+    dow = dayOfWeek(y, m, d);
+    console.log(dow)
+    if(dow == 6){
+      fill(0,0,255);
+    }
+    else if(dow == 0){
+      fill(255,0,0);
+    }
+
+    textSize(14);
+    if(d < 10){
+      text(d, (dow * 25 + p + 4), t + 70 );
+    }
+    else text(d, (dow * 25 + p ), t + 70  );
+    fill(0);
+    if(dow ==6){
+       t += 25;
+         }
+  }
+
 }
 
 function isLeapYear(y){
@@ -25,7 +74,7 @@ function isLeapYear(y){
 }
 
 function daysInYear(y){
-  // BLANK[1]
+  return isLeapYear(y) ? 366 : 365;
 }
 
 function daysInMonth(y, m){
@@ -42,17 +91,24 @@ function daysInMonth(y, m){
 
 function dayOfYear(y, m, d){
   let count = 0;
+  for(let t = 1970; t < y; t++){
+    count += daysInYear(t)
+  }
   for(let i = 1; i < m; i++){
     count += daysInMonth(y, i);
   }
-  return count + d;
+  return count + d - 1;
 }
 
 function dayOfWeek(y, m, d){
-  // BLANK[2]
+  let a =  dayOfYear(y, m, d) % 7;
+  console.log(a);
+  if(a >= 0 && a <= 2) return a+4;
+  else return a-3;
+
 }
 
 function dayOfWeekAsString(dow){
-  const a = ["日", "月", "火", "水", "木", "金", "土", "日"];
+  const a = ["日", "月", "火", "水", "木", "金", "土"];
   return a[dow];
 }

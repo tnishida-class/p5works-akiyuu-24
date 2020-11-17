be42e13e1a9aee0308a14993acf65035e3c47a89
akiyuu-24 <72780570+akiyuu-24@users.noreply.github.com>
2020-11-17 15:44:02 +0900

    changesection4-q2

--- section4-q2/sketch.js
+++ section4-q2/sketch.js
@@ -1,17 +1,23 @@
 // テキスト「アニメーション」
-let x, y, vx, vy;
+let x, y, vx, vy, angle;
 
 function setup(){
   createCanvas(windowWidth, windowHeight);
   x = width / 2;
   y = height / 2;
-  vx = 8;
-  vy = 8;
+  vx = 5;
+  vy = 5;
+  angle = 0;
 }
 
 function draw(){
   background(160, 192, 255);
   // BLANK[2] (hint: 作った star 関数を使います)
+  fill(255,255,0);
+  star(x, y, 30, angle);
+  x += vx;
+  y += vy;
+  angle += 1 ;
 
   // 端の処理パターン (1) 反対側から出てくる
   if(x > width){ x = 0; }
@@ -22,9 +28,9 @@ function draw(){
 
 function star(cx, cy, r, angle){
   beginShape();
-  for(var i = 0; i < 20; i++){
-    var theta = TWO_PI * i * 2 / 5 - HALF_PI;
-    // BLANK[1] (hint: angle 分だけ星を回転させるには？)
+  for(var i = 0; i < 5; i++){
+    var theta = TWO_PI * i * 2 / 5 - ( HALF_PI + angle )
+    // BLANK[1] (hint: angle 分だけ星を回転させるに
     var x = cx + cos(theta) * r;
     var y = cy + sin(theta) * r;
     vertex(x,y);
